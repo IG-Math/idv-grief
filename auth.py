ca1525eafcec83176e6ae85007ddd4e2cefa49ce
IG-Math <147543283+IG-Math@users.noreply.github.com>
2026-01-04 14:24:50 +0900

    auth.py を更新
    Use SHA256

--- auth.py
+++ auth.py
@@ -16,7 +16,7 @@ if not SECRET_KEY:
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a password against a hash"""
